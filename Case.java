08766a3eeac9016f6d112e97a486a47a6fe2e146
Zakaria Makhlouf <ton.email@example.com>
2025-03-14 11:12:08 +0100

    d

--- Case.java
+++ Case.java
@@ -1,3 +1,45 @@
 public class Case {
+    private boolean contientUneBombe;
+    private boolean estDecouverte;
+    private boolean estMarquee;
 
+    public Case() {
+        this.contientUneBombe = false;
+        this.estDecouverte = false;
+        this.estMarquee = false;
+    }
+
+    public void reset() {
+        this.contientUneBombe = false;
+        this.estDecouverte = false;
+        this.estMarquee = false;
+    }
+
+    public void poseBombe() {
+        this.contientUneBombe = true;
+    }
+
+    public boolean contientUneBombe() {
+        return this.contientUneBombe;
+    }
+
+    public boolean estDecouverte() {
+        return this.estDecouverte;
+    }
+
+    public boolean estMarquee() {
+        return this.estMarquee;
+    }
+
+    public boolean reveler() {
+        if (!this.estDecouverte) {
+            this.estDecouverte = true;
+            return true;
+        }
+        return false;
+    }
+
+    public void marquer() {
+        this.estMarquee = !this.estMarquee;
+    }
 }
